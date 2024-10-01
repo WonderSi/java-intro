36863d56cd15347c471b7999bb2400ac2d850933
WonderSi <kirillkozlov00@gmail.com>
2024-10-01 21:07:27 +0700

    Add Test Button

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,5 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    @Test
+    public void testInitialClickCount() {
+        Button button = new Button();
+        assertEquals(0, button.getNumberClick(), "Initial click count should be 0");
+    }
 
+    @Test
+    public void testResetClickCount() {
+        Button button = new Button();
+        button.click();
+        button.reset();
+        button.click();
+        button.click();
+        assertEquals(2, button.getNumberClick(), "Click count should be 2 after reset");
+    }
 }
