838a02f3ed48dcebdba8d49d6bd6902783d3ecb2
WonderSi <kirillkozlov00@gmail.com>
2024-10-09 01:01:49 +0700

    Add Number 4

---
+++ src/main/java/org/example/FourthTask.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourthTask {
+    public static void main(String[] args) {
+        String text = "This is an example. This example is simple.";
+
+        // Преобразуем текст в нижний регистр и разбиваем на слова
+        String[] words = text.toLowerCase().replaceAll("[^a-zA-Z ]", "").split("\\s+");
+
+        // Подсчет частоты слов
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        for (String word : words) {
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println("Частота слов: " + wordCountMap);
+    }
+}
