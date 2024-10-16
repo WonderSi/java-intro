82716596b8834bce4f574654be58143837d5160d
WonderSi <kirillkozlov00@gmail.com>
2024-10-16 23:33:22 +0700

    Add Number 1 and 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation ("com.google.code.gson:gson:2.8.9")
+
+    compileOnly ("org.projectlombok:lombok:1.18.30")
+    annotationProcessor ("org.projectlombok:lombok:1.18.30")
 }
 
 tasks.test {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,44 @@
 package org.example;
 
-import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.Data;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+@Data
+class Book {
+    private String name;
+    private String author;
+    private int publishingYear;
+    private String isbn;
+    private String publisher;
+}
+
+@Data
+class Visitor {
+    private String name;
+    private String surname;
+    private String phone;
+    private boolean subscribed;
+    private List<Book> favoriteBooks;
+}
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+        Gson gson = new Gson();
+        List<Visitor> visitors = gson.fromJson(new FileReader("src/main/resources/books.json"), new TypeToken<List<Visitor>>() {
+        }.getType());
 
-        
-        int number = scanner.nextInt();
+        //1
+        System.out.println("Посетители:");
+        visitors.forEach(v -> System.out.println(v.getName() + " " + v.getSurname()));
+        System.out.println("Количество посетителей: " + visitors.size());
 
-        System.out.println(inc(number));
-    }
-
-    public static int inc(int number) {
-        return number + 1;
+        //2
+        long uniqueBooksCount = visitors.stream().flatMap(v -> v.getFavoriteBooks().stream()).distinct().count();
+        System.out.println("Количество уникальных книг в избранном: " + uniqueBooksCount);
     }
 }
