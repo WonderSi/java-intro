223c2fdcc02acf4a2e6cba6c96ddd6207110924d
WonderSi <kirillkozlov00@gmail.com>
2024-10-22 01:16:05 +0700

    Add Number 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Comparator;
 import java.util.List;
 
 @Data
@@ -33,12 +34,24 @@ public class Main {
         }.getType());
 
         //1
+        System.out.println("\nЗадание 1");
         System.out.println("Посетители:");
         visitors.forEach(v -> System.out.println(v.getName() + " " + v.getSurname()));
         System.out.println("Количество посетителей: " + visitors.size());
 
         //2
+        System.out.println("\nЗадание 2");
         long uniqueBooksCount = visitors.stream().flatMap(v -> v.getFavoriteBooks().stream()).distinct().count();
         System.out.println("Количество уникальных книг в избранном: " + uniqueBooksCount);
+
+        //3
+        System.out.println("\nЗадание 3");
+        List<Book> sortedBooks = visitors.stream()
+                .flatMap(v -> v.getFavoriteBooks().stream())
+                .sorted(Comparator.comparingInt(Book::getPublishingYear))
+                .toList();
+        System.out.println("Список книг (отсортированных по году издания): ");
+        sortedBooks.forEach(book -> System.out.println(book.getName() + " (" + book.getPublishingYear() + ")"));
+
     }
 }
